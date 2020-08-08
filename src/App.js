5887c34f60d4bde1a16db44a199731e59630042a
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-08 21:42:35 +0100

    Importing <Provider/> from react-redux and wrapping it around our <App/>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
+class App extends Component {
+
+  render() {
+
+    return(
+      <div>
+        Hello
+      </div>
+    )
+  }
 }
 
 export default App;
