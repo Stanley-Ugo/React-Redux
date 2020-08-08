1152b86c97cb65ea2419ec4d0d12840df499301a
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-09 00:46:20 +0100

    adding axios & making a request to the server

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,17 +2,15 @@ import {
     MOVIES_LIST,
 } from '../types'
 
+import axios from 'axios';
 
 export function moviesList(){
 
-    
+    const request = axios.get('http://jsonplaceholder.typicode.com/posts')
+                    .then( response => response.data )
 
     return {
         type: MOVIES_LIST,
-        payload: [
-            {id:1, name: "Pulp Fiction"},
-            {id:2, name: "Pacific Rim"},
-            {id:3, name: "Rambo"}
-        ]
+        payload: request
     }
 }
